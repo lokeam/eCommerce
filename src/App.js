89a169de044c7aa2b5d696c3e528769248eaa2fd
Ahnming Loke <lokeahnming@gmail.com>
2019-12-15 17:10:19 -0500

    feat: wire up main menu items to ReactRouterDom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route exact path='/' component={ Homepage } />
+        <Route exact path='/hats' component={ HatsPage } />
+      </Switch>
       <Homepage />
     </div>
   );
