d95d45df130db57a0c95479b7ed1a87c94ac2c89
Ahnming Loke <lokeahnming@gmail.com>
2019-12-16 15:21:55 -0500

    feat: add desktop navigation component with basic nav links

---
+++ src/components/navigation/navigation.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { ReactComponent as Logo } from '../../static/svg/logo.svg';
+import { Link } from 'react-router-dom';
+
+import styles from './navigation.styles.scss';
+
+const Navigation = () => (
+  <div className='nav'>
+    <Link to='/' className='logo__wrapper' to='/'>
+      <Logo className='logo' />
+    </Link>
+    <div className='nav__linkContainer'>
+      <Link to='/shop' className='nav__linkWrapper'>
+        shop
+      </Link>
+      <Link to='/contact' className='nav__linkWrapper'>
+        contact
+      </Link>
+    </div>
+  </div>
+);
+
+export default Navigation;
