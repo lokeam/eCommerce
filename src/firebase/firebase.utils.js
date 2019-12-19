891dc64f1706ff4f632331c637115b80ef054515
Ahnming Loke <lokeahnming@gmail.com>
2019-12-18 21:56:14 -0800

    feat: implement storing user data in Firebase and app

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -5,6 +5,30 @@ import 'firebase/auth';
 
 firebase.initializeApp(FIREBASE_CONFIG);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if ( !userAuth ) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const creationDate = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        creationDate,
+        ...additionalData
+      });
+    } catch (error) {
+    console.log('error creating user ', error.message);
+    }
+  }
+  return userRef;
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
