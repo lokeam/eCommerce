eac1a840049830735863df66ffec0f807cf20f96
Ahnming Loke <lokeahnming@gmail.com>
2019-12-19 16:31:57 -0800

    feat: added sign-up component, enabled email userAuth with Firebase

--- src/pages/login/login-page.component.jsx
+++ src/pages/login/login-page.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 import styles from './login-page.styles.scss';
 
 const LogInPage = () => (
-  <div className="signin">
+  <div className="login-page">
     <SignIn />
+    <SignUp />
   </div>
 );
 
