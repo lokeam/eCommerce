ba8bf24b1c8fc0188203c250f779e47bf9f3294e
Ahnming Loke <lokeahnming@gmail.com>
2019-12-31 15:18:05 -0500

    feat: added checkout page and checkout item component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect'; 
+import { withRouter } from 'react-router-dom';
 
 import CTABtn from '../cta-btn/cta-btn.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
-      {cartItems.map(cartItem => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))}
+      {
+        cartItems.length ? (
+        cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+        ):(
+          <span className='empty-message'>Your cart is empty</span>
+        )}
     </div>
-    <CTABtn>GO TO CHECKOUT</CTABtn>
+    <CTABtn onClick={() =>{
+      history.push('/checkout');
+      dispatch(toggleCartHidden());
+    }} >GO TO CHECKOUT</CTABtn>
   </div>
 );
 
@@ -23,4 +33,4 @@ const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
