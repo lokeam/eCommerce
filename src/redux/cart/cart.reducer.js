e788f151a0e0785a0fb4c10f43931729fc6788b9
Ahnming Loke <lokeahnming@gmail.com>
2020-01-01 14:00:19 -0500

    feat: implemented quantity increase and decrease feature to checkout item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,5 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
-import { addItem } from './cart.actions';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -13,12 +12,18 @@ const cartReducer = ( state = INITIAL_STATE, action ) => {
     return {
       ...state,
       hidden: !state.hidden
-    }
+    };
 
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
       };
 
     case CartActionTypes.CLEAR_ITEM_FROM_CART:
@@ -27,7 +32,7 @@ const cartReducer = ( state = INITIAL_STATE, action ) => {
         cartItems: state.cartItems.filter(
           cartItem => cartItem.id !== action.payload.id
         )
-      }
+      };
 
     default:
       return state;
