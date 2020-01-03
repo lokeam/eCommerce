c5cebcb9daaa11723b35824a64acedf6863d3e56
Ahnming Loke <lokeahnming@gmail.com>
2020-01-03 11:09:27 -0500

    refactor: move collections map from shop page into its own component

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PreviewCollection from '../preview-collection/preview-collection.component';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCollections } from '../../redux/shop/shop.selectors';
+import './collections-overview.styles.scss';
+
+const CollectionsOverview = ({ collections }) => (
+  <div className="collections-overview">
+    {collections.map( ({id, ...otherCollectionProps}) => (
+      <PreviewCollection key={collections.id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -6,7 +6,7 @@ const PreviewCollection = ({ title, items }) => (
   <div className="collection__wrapper">
     <h1 className="collection__title"> { title } </h1>
     <div className="collection__preview">
-      { /* Performance concern. WIll refactor later */ }
+      { /* Performance concern. Will refactor later */ }
       { items
         .filter( (item, index) => index < 4 )
         .map( ( item ) => (
